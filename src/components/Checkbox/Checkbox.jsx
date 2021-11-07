cb1703907d0a3d905d2ae3f2fa1d7a7f9d464cad
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-07 20:24:57 +0100

    Refactor conditionals

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -2,8 +2,8 @@ import "./Checkbox.css";
 
 const Checkbox = (props) => {
     return <div className="checkbox">
-        <div className={`checkbox__box ${props.checked === true ? "checkbox__box--checked" : ""}`}/>
-        <span className={`checkbox__text ${props.checked === true ? "checkbox__text--checked" : ""}`}>
+        <div className={`checkbox__box ${props.checked ? "checkbox__box--checked" : ""}`}/>
+        <span className={`checkbox__text ${props.checked ? "checkbox__text--checked" : ""}`}>
             {props.children}
         </span>
     </div>
