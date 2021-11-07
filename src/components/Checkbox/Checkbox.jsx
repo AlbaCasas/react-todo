1c3d9dde0455a5e32425b3dff08a8f1781efce9f
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-07 20:14:42 +0100

    Create Checkbox components

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -2,8 +2,8 @@ import "./Checkbox.css";
 
 const Checkbox = (props) => {
     return <div className="checkbox">
-        <div className="checkbox__box checkbox__box--unchecked"/>
-        <span>
+        <div className={`checkbox__box ${props.checked === true ? "checkbox__box--checked" : ""}`}/>
+        <span className={`checkbox__text ${props.checked === true ? "checkbox__text--checked" : ""}`}>
             {props.children}
         </span>
     </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 import Checkbox from "../components/Checkbox";
 
 function App(props) {
-  return <Checkbox>hola</Checkbox>
+  return <Checkbox checked={false}>Lorem ipsum dolor sit amet.</Checkbox>
 }
 
 export default App;
