d038655e5d05fc0d5562d9f71d92e7b07cd34b5f
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-11 15:08:56 +0100

    Create task list in App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,13 +2,34 @@ import Task from "../components/Task";
 import Input from "../components/Input/";
 import "./App.css";
 
+const tasks = [
+  {
+    date: "Monday",
+    checked: false,
+    text: "Do the math for next monday.",
+  },
+  {
+    date: null,
+    checked: true,
+    text: "Send the finished assignment",
+  },
+  {
+    date: "Yesterday",
+    checked: true,
+    text: "Read english book chapter 5",
+  },
+];
 const App = (props) => {
   return (
     <div className="app">
-      <Task checked={false} date="Monday">
-        Do the math for next monday.
-      </Task>
       <Input placeholder="Add a task" />
+      {tasks.map((task) => {
+        return (
+          <Task date={task.date} checked={task.checked}>
+            {task.text}
+          </Task>
+        );
+      })}
     </div>
   );
 };
